58c75cbb29069ba206c26f172b8d645f442ea55d
Avishek Sen Gupta <avishek.sen.gupta@gmail.com>
2014-02-23 01:25:32 -0500

    Showing why scope.() is needed - <Mojo>

--- example-app/src/main/resources/public/js/test/unit/directives/directive1-test.js
+++ example-app/src/main/resources/public/js/test/unit/directives/directive1-test.js
@@ -5,6 +5,9 @@ define(["spec_helper"], function(mother) {
 			var scope = {phoneNumber: "1234567890"};
 			return mother.compileDirective(template, scope).spread(function(s, element) {
 				expect(element.text()).to.eql("(123) 456-7890");
+				s.phoneNumber = "5671234400";
+				s.$apply();
+				expect(element.text()).to.eql("(567) 123-4400");
 			});
 		});
 	});
